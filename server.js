e19634d5d758b1695d94ac260dd4642b903d93c0
IgorLAO <igoli6767@gmail.com>
2025-08-02 11:07:54 -0300

    testess

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const port = 4002;
 app.get('/', (req, res) => res.send('viado gay kkkkkkkk'))
 
 app.listen(port, '0.0.0.0',() => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}` );
 
 })
