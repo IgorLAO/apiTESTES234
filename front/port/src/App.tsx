96b423ae9450fc7c18eda7f061efa9a240d2d475
IgorLAO <igoli6767@gmail.com>
2025-08-10 14:11:51 -0300

    arrumando testes

--- front/port/src/App.tsx
+++ front/port/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import reactLogo from './assets/ab529c_9c02d08edc58457a9977d4f891628176~mv2.avif'
 import anti from './assets/images.jpeg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
